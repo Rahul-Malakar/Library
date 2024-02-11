079bd9d430783ec865c825b7427c1d0d5948e18c
rahulmalakar42 <malakarahul111@gmail.com>
2024-02-12 00:23:10 +0530

    jwt complete

--- backend/app.js
+++ backend/app.js
@@ -11,12 +11,17 @@ const Member = require('./models/member')
 const bookroutes = require('./routes/bookroutes')
 const memberroutes = require('./routes/memberroutes')
 const authroutes = require('./routes/authroutes')
+//import cookie-parser
+const cookieparser = require('cookie-parser')
+//auth middleware
+const { requireAuth, checkUser } = require('./middleware/authmiddleware');
 
 //express app
 const app = express();
 
 //frontend reference
-const path = require('path')
+const path = require('path');
+const cookieParser = require('cookie-parser');
 app.set('view engine', 'ejs');
 app.set('views', [path.join(__dirname, '..', 'frontend', 'views'),path.join(__dirname, '..', 'frontend', 'views','books'),path.join(__dirname, '..', 'frontend', 'views', 'members'),path.join(__dirname, '..', 'frontend', 'views','auth')]);
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'public')));
@@ -25,15 +30,33 @@ app.use(express.static(path.join(__dirname, '..', 'frontend', 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 //base url
+app.get('*', checkUser)
 app.get('/', (req, res) => {
   res.redirect('/books');
 });
 
+//cookies
+// app.get('/set-cookies', (req,res)=>{
+//   // res.setHeader('Set-Cookie', 'newUser=true');
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, {maxAge: 1000*60*60*24});
+//   res.send('you got some cookies')
+// })
+
+// app.get('/read-cookies', (req,res)=>{
+//   const cookies = req.cookies;
+//   console.log(cookies);
+//   res.json(cookies);
+// })
+
+
 //use routes
+
 app.use(bookroutes);
-app.use(memberroutes);
+app.use('/members',requireAuth,memberroutes);
 app.use(authroutes)
 
 //404 page
--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,15 +1,49 @@
 const User = require('../models/user')
 const statuscode = require('../statuscode')
+const jwt = require('jsonwebtoken')
 
 //handle errors
 const handleErrors = (err)=>{
     console.log(err.message, err.code);
-    let error = {email: '', password: ''};
+    let errors = {email: '', password: ''};
+
+    //incorrect email
+    if(err.message === 'incorrect email'){
+        errors.email = 'email is not registered'
+    }
+    //incorrect password
+    if(err.message === 'incorrect password'){
+        errors.password = 'password is incorrect'
+    }
+
+    //duplicate
+    if(err.code===11000){
+        errors.email = 'already registered';
+        return errors;
+    }
 
     //validation errors
+    // if(err.message.includes('User validation failed')){
+    //     (Object.values(err.errors)).forEach(({properties}) =>{
+    //         console.log(properties)
+    //     })
+    // }
+    
     if(err.message.includes('User validation failed')){
-        console.log(err)
+        (Object.values(err.errors)).forEach(({properties}) =>{
+            errors[properties.path] = properties.message;
+        })
     }
+
+    return errors;
+}
+
+
+const maxAge = 3*24*60*60;
+const createToken = (id) =>{
+    return jwt.sign({id}, 'rahul secret', {
+        expiresIn: maxAge
+    })
 }
 
 const signupGet = (req,res)=>{
@@ -19,28 +53,45 @@ const signupPost = async (req,res)=>{
     const {email,password} = req.body;
     try{
         const user = await User.create({email,password});
-        res.status(success).json(user);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge*1000})
+        res.status(success).json({user : user._id});
     }
     catch(err){
-        handleErrors(err);
-        res.status(failed).send('error, user not created');
+        const errors = handleErrors(err);
+        res.status(failed).json({errors});
     }
 }
 const loginGet = (req,res)=>{
-    res.render('login', {title:'login'})
+    const notLoggedIn = req.query.notLoggedIn === 'true';
+
+  // Render your login page and pass notLoggedIn to the view
+  res.render('login', {title: 'login', notLoggedIn });
 }
 const loginPost = async (req,res)=>{
+    const {email,password} = req.body;
     try{
-
+        const user = await User.login(email,password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge*1000})
+        res.status(success).json({user: user._id})
     }
     catch(err){
-
+        const errors = handleErrors(err)
+        res.status(failed).json({errors});
     }
+
+}
+
+const logoutget = (req,res)=>{
+    res.cookie('jwt', '', {maxAge: 1});
+    res.redirect('/login')
 }
 
 module.exports = {
     signupGet,
     signupPost,
     loginGet,
-    loginPost
+    loginPost,
+    logoutget
 }
